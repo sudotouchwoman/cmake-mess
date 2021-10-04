a72dad3d35246e9cdac9a68005243e512bc68473
Nikita Teterin <nekitonum@gmail.com>
2021-10-04 16:41:22 +0300

    update lib and CMakeLists.txt

--- src/test-lib/test-lib.cpp
+++ src/test-lib/test-lib.cpp
@@ -4,12 +4,12 @@ const std::string message_from_src(){
     return "Hi there from test-lib.cpp\n";
 }
 
-const int multiply(const int a, const int b){
+int multiply(const int a, const int b){
     const int result = a * b;
     return result;
 }
 
-const int add(const int a, const int b){
+int add(const int a, const int b){
     const int result = a + b;
     return result;
 }
